6a1c80e030d1cc9a21be59f0f38dd6b9df49e273
wishkalalai <30793685+wishkalalai@users.noreply.github.com>
2017-08-08 16:24:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,12 @@ element.innerHTML = "Kalai here!!!";
 
 var img = document.getElementById("madi");
 
-marginLeft = 0;
+var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + "px";
 }
 
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 }
