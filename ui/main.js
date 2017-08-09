fffcc5deb4cbc1e00c8556a87172a0d7937289e1
wishkalalai <30793685+wishkalalai@users.noreply.github.com>
2017-08-09 12:58:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
-element.innerHTML = "Kalai here!!!";
+//element.innerHTML = "Kalai here!!!";
 
 var img = document.getElementById('madi');
 
